3b1fd4590f24d7ae509ee3ff439a861cdc798d3d
skedwards88 <skedwards88@github.com>
2023-09-08 08:10:42 -0700

    fix easy vs hard bug

--- src/logic/letterPool.js
+++ src/logic/letterPool.js
@@ -1,12 +1,15 @@
 import {getLetterPool} from "@skedwards88/word_logic";
 import {
+  commonWordsLen3,
   commonWordsLen4,
   commonWordsLen5,
   commonWordsLen6,
   commonWordsLen7,
 } from "@skedwards88/word_lists";
 
+// Just using the shorter common words since this is what people tend to find
 export const letterPool = getLetterPool([
+  ...commonWordsLen3,
   ...commonWordsLen4,
   ...commonWordsLen5,
   ...commonWordsLen6,
--- src/logic/trie.js
+++ src/logic/trie.js
@@ -1,17 +1,4 @@
 import {getTrie} from "@skedwards88/word_logic";
-import {
-  commonWordsLen4,
-  commonWordsLen5,
-  commonWordsLen6,
-  commonWordsLen7,
-} from "@skedwards88/word_lists";
+import {commonWords, uncommonWords} from "@skedwards88/word_lists";
 
-export const trie = getTrie(
-  [
-    ...commonWordsLen4,
-    ...commonWordsLen5,
-    ...commonWordsLen6,
-    ...commonWordsLen7,
-  ],
-  [],
-);
+export const trie = getTrie([...commonWords], [...uncommonWords]);
